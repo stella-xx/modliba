efc8f6c6e7ab321461ab1037e0136054808072c4
hanxiao1 <hanxiao1@guazi.com>
2019-05-16 08:13:24 +0800

    new mod lib a

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/stella-xx/modlia
+
+go 1.12
+
+require github.com/stella-xx/modlib v1.1.0
---
+++ user.go
@@ -0,0 +1,8 @@
+package modliba
+
+import "github.com/stella-xx/modlib/lib"
+
+func Read() {
+	user := lib.NewUser("Sharon")
+	user.Read()
+}
